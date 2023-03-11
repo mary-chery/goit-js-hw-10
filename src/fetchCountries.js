30cf84bba16ac1d763cf8ac9c41c65c9af28659d
mary-chery <cherkashina.maryna@gmail.com>
2023-03-11 12:41:33 +0200

    hw10

---
+++ src/fetchCountries.js
@@ -0,0 +1,17 @@
+export { fetchCountries };
+
+//     function fetchCountries(name) {
+//     return fetch('https://restcountries.com/v3.1/name/${name}?fiels=name,capital,population,flags.svg,languages')
+//         .then(response => response.json());
+// }
+
+function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fiels=name,capital,population,flags.svg,languages`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
