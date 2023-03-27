2aa81d5a3ffd3353705ecf36e16179f96df12fa3
mary-chery <cherkashina.maryna@gmail.com>
2023-03-27 16:15:08 +0300

    test commit

--- src/calendar.js
+++ src/calendar.js
@@ -9,6 +9,7 @@ const app = document.querySelector('input#app');
 const icon = document.querySelector('.icon-Vector-1');
 const icon2 = document.querySelector('.icon-Vector');
 
+
 app.addEventListener('click', onClick);
 
 function onClick() {
@@ -17,7 +18,7 @@ function onClick() {
     icon.classList.add('icon-vector-open');
     icon2.classList.add('icon-vector-open-2');
   }
-  // else if () {
+  // else if (app.classList !== 'active') {
   //   app.classList.remove('input-focus')
   //   icon.classList.remove('icon-vector-open');
   //   icon2.classList.remove('icon-vector-open-2');}
@@ -25,6 +26,18 @@ function onClick() {
   }
 
 
+// function onOpen() {
+//   app.classList.add('input-focus')
+//     icon.classList.add('icon-vector-open');
+//     icon2.classList.add('icon-vector-open-2');
+// }
+
+// function onClose() {
+  
+//     app.classList.remove('input-focus')
+//     icon.classList.remove('icon-vector-open');
+//     icon2.classList.remove('icon-vector-open-2');
+// }
 const options = {
   locale: {
         firstDayOfWeek: 1
@@ -32,9 +45,24 @@ const options = {
 
   defaultDate: new Date(),
 
-    dateFormat: "d/m/Y",
+  dateFormat: "d/m/Y",
+
+  
+    
+  onClose(selectedDates) {
+    if (selectedDates) {
+    app.classList.remove('input-focus')
+    icon.classList.remove('icon-vector-open');
+    icon2.classList.remove('icon-vector-open-2');
+    } 
+  },
+  onChange(selectedDates) {
+    if (selectedDates[0].getTime() > new Date().getTime()) {
+
+      alert('🥺 Ooops..., Please, choose a date in the past')
+    }
+  },
 
 };
 
 flatpickr(app, options);
-
